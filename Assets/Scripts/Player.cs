e98ba5a479327aa1ab848b9c28957080bbf50a8a
Linda Pizzato <linda.pizzato03@htl-salzburg.ac.at>
2021-03-22 13:04:06 +0100

    Aufgabe Local Multiplayer Shooter

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public KeyCode right;
+    public KeyCode left;
+    public KeyCode jump;
+    public Rigidbody r;
+    public Vector3 jumpPower;
+    public Vector3 rightPower;
+    public Vector3 leftPower;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (Input.GetKeyDown(jump))
+        {
+            r.AddForce(jumpPower);
+        }
+
+        if (Input.GetKeyDown(right))
+        {
+            print("hi");
+            r.AddForce(rightPower);
+        }
+    
+        if (Input.GetKeyDown(left))
+        {
+            print("no");
+            r.AddForce(leftPower);
+        }
+    }
+}
